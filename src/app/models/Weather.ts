d001d76dc0b62f7c649f7dd2a6eb217887d6901b
Pierre Boudeaud <pboudeaud@gmail.com>
2019-03-01 16:12:58 +0100

    Add weather model

---
+++ src/app/models/Weather.ts
@@ -0,0 +1,11 @@
+export class Weather {
+  temperature: number;
+  windSpeed: number;
+  pression: number;
+  humidity: number;
+  description: string;
+  icon: string;
+  i18nContext: string;
+  sunrise: Date;
+  sunset: Date;
+}
